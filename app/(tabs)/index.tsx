867bdecb0cd39134f76345bf413c60791e06574d
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-08-07 22:00:49 -0400

    create loading spinner

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,23 @@
 import { createHomeStyles } from "@/assets/styles/home.styles";
 import Header from "@/components/Header";
+import LoadingSpinner from "@/components/LoadingSpinner";
 import TodoInput from "@/components/TodoInput";
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
+import { useQuery } from "convex/react";
 import { LinearGradient } from "expo-linear-gradient";
-import { StatusBar, Text, TouchableOpacity } from "react-native";
+import { StatusBar, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const { toggleDarkMode, colors } = useTheme();
 
   const homeStyles = createHomeStyles(colors);
+  const todos = useQuery(api.todos.getTodos);
+  const isLoading = todos === undefined;
+
+  if (isLoading) return <LoadingSpinner />;
+
   return (
     <LinearGradient
       colors={colors.gradients.background}
@@ -19,9 +27,8 @@ export default function Index() {
       <SafeAreaView style={homeStyles.safeArea}>
         <Header />
         <TodoInput />
-        <TouchableOpacity onPress={toggleDarkMode}>
-          <Text>Toggle</Text>
-        </TouchableOpacity>
+
+        {todos?.map((todo) => <Text key={todo._id}>{todo.text}</Text>)}
       </SafeAreaView>
     </LinearGradient>
   );
