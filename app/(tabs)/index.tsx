8bae2961be293d250135f3146395cf966c7ab54d
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-08-07 21:18:02 -0400

    create a function to handle style dyna from the usetheme store

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,40 +1,31 @@
-import { api } from "@/convex/_generated/api";
-import useTheme from "@/hooks/useTheme";
-import { useMutation, useQuery } from "convex/react";
+import useTheme, { ColorScheme } from "@/hooks/useTheme";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
-  const { toggleDarkMode } = useTheme();
+  const { toggleDarkMode, colors } = useTheme();
 
-  const todos = useQuery(api.todos.getTodos);
-  console.log(todos);
-
-  const addTodo = useMutation(api.todos.addTodo);
-  const clearAllTodos = useMutation(api.todos.clearAllTodos);
-
+  const styles = createStyles(colors);
   return (
     <View style={styles.container}>
       <Text style={styles.content}>Naole</Text>
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>Toggle</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => addTodo({ text: "walk the dog" })}>
-        <Text>add a new todo</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => clearAllTodos()}>
-        <Text>clear all todo</Text>
-      </TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  content: {
-    fontSize: 52,
-  },
-});
+const createStyles = (colors: ColorScheme) => {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: colors.bg,
+    },
+    content: {
+      fontSize: 52,
+    },
+  });
+  return styles;
+};
