378757c263e429c0018c2b5f62ef9a140345b6af
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-08-10 19:12:50 -0400

    finished the progress component and workin as exp

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,9 +1,10 @@
 import { createSettingsStyles } from "@/assets/styles/settings.styles";
+import ProgressStats from "@/components/ProgressStats";
 import useTheme from "@/hooks/useTheme";
 import { Ionicons } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
 import React, { useState } from "react";
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const settingsScreen = () => {
@@ -30,6 +31,14 @@ const settingsScreen = () => {
           </View>
         </View>
       </SafeAreaView>
+
+      <ScrollView
+        style={settingsStyles.scrollView}
+        contentContainerStyle={settingsStyles.content}
+        showsVerticalScrollIndicator={false}
+      >
+        <ProgressStats />
+      </ScrollView>
     </LinearGradient>
   );
 };
